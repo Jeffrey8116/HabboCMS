0d4681f69d0bf45b4f038beccd63865562ab4f61
Jeffrey <jeffrey8116@live.com>
2015-03-19 20:10:22 -0400

    v1

--- vote/Index.php
+++ vote/Index.php
@@ -1,1 +1,18 @@
-Index
+<?php
+require_once 'config.php';
+require_once 'core.php';
+
+$FindRetros = new FindRetros();
+
+if($FindRetros->hasClientVoted()) {
+
+    echo 'You have voted!';
+
+}else{
+
+    // echo 'You have yet to vote!';
+
+    $FindRetros->redirectClientToVote();
+
+}
+?>
---
+++ vote/config.php
@@ -0,0 +1,7 @@
+<?php
+$_CONFIG = array(
+    'pagename' => 'Example', //Page Name Ex 'FindRetros'
+    'timeout'  => 2, // How much seconds you want to reach FR if its down
+    'cloudflare' => false, // Set true if you are using cloudflare
+    'api' => 'https://findretros.com/' //Only change if Findretros isn't the domain anymore.
+);
